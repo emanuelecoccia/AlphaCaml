d8384864461c2156bbd2c2aa9c5e0f37d0b42ce6
Emanuele Coccia <emanuele.coccia@studbocconi.it>
2025-11-09 23:04:52 +0100

    add validate and print position

--- lib/position.ml
+++ lib/position.ml
@@ -1,20 +1,48 @@
 open Core
 
-module Square = struct
-  module T = struct
-    type t = { file : int; rank : int } [@@deriving sexp, compare, hash]
-  end
-
-  include T
-  include Comparable.Make (T)
-end
-
 module Board = struct
   type t = Piece.t option Square.Map.t [@@deriving sexp]
+
+  let validate (board : t) =
+    Map.iter_keys board ~f:(fun square ->
+        if not (Square.is_valid square) then
+          raise_s [%message "Square out of bounds: " (square : Square.t)]);
+
+    let expected =
+      List.cartesian_product (List.range 0 8) (List.range 0 8)
+      |> List.map ~f:(fun (file, rank) -> Square.of_tuple (file, rank))
+      |> Square.Set.of_list
+    in
+    let actual = board |> Map.keys |> Square.Set.of_list in
+    if not (Square.Set.equal expected actual) then
+      failwith "Board is missing squares or has extra squares";
+    board
+
+  let print (t : t) =
+    let print_square square =
+      match Map.find_exn t square with
+      | None -> "."
+      | Some piece -> Piece.to_ascii piece
+    in
+    List.iter (List.range 0 8) ~f:(fun rank ->
+        List.iter (List.range 0 8) ~f:(fun file ->
+            let square = Square.of_tuple (file, rank) in
+            printf "%s " (print_square square));
+        printf "\n")
+
+  let t_of_sexp sexp =
+    let board = t_of_sexp sexp in
+    print board;
+    validate board
 end
 
 type t = { board : Board.t; turn : Color.t } [@@deriving sexp]
 
 let init () : t =
-  In_channel.with_file "data/initial_position.sexp" ~f:(fun ic ->
+  In_channel.with_file "positions/init.sexp" ~f:(fun ic ->
       Sexplib.Sexp.input_sexp ic |> t_of_sexp)
+
+let print_position (pos : t) =
+  Board.print pos.board;
+  printf "%s to move. \n"
+    (match pos.turn with White -> "White" | Black -> "Black")
--- lib/position.mli
+++ lib/position.mli
@@ -1,3 +1,4 @@
 type t [@@deriving sexp]
 
 val init : unit -> t
+val print_position : t -> unit
