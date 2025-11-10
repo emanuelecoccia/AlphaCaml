c823d1e988c24b453beb6074d57bf64d651d783c
Emanuele Coccia <emanuele.coccia@studbocconi.it>
2025-11-10 01:10:58 +0100

    changed piece type from variant to record

--- lib/piece.ml
+++ lib/piece.ml
@@ -1,22 +1,19 @@
-type t =
-  | Pawn of Color.t
-  | Knight of Color.t
-  | Bishop of Color.t
-  | Rook of Color.t
-  | Queen of Color.t
-  | King of Color.t
-[@@deriving sexp]
+module Kind = struct
+  type t = Pawn | Knight | Bishop | Rook | Queen | King [@@deriving sexp]
+end
+
+type t = { kind : Kind.t; color : Color.t } [@@deriving sexp, fields ~getters]
 
 let to_ascii = function
-  | Pawn White -> "♙"
-  | Knight White -> "♘"
-  | Bishop White -> "♗"
-  | Rook White -> "♖"
-  | Queen White -> "♕"
-  | King White -> "♔"
-  | Pawn Black -> "♟"
-  | Knight Black -> "♞"
-  | Bishop Black -> "♝"
-  | Rook Black -> "♜"
-  | Queen Black -> "♛"
-  | King Black -> "♚"
+  | { kind = Pawn; color = White } -> "♙"
+  | { kind = Pawn; color = Black } -> "♟"
+  | { kind = Knight; color = White } -> "♘"
+  | { kind = Knight; color = Black } -> "♞"
+  | { kind = Bishop; color = White } -> "♗"
+  | { kind = Bishop; color = Black } -> "♝"
+  | { kind = Rook; color = White } -> "♖"
+  | { kind = Rook; color = Black } -> "♜"
+  | { kind = Queen; color = White } -> "♕"
+  | { kind = Queen; color = Black } -> "♛"
+  | { kind = King; color = White } -> "♔"
+  | { kind = King; color = Black } -> "♚"
--- lib/piece.mli
+++ lib/piece.mli
@@ -1,10 +1,7 @@
-type t =
-  | Pawn of Color.t
-  | Knight of Color.t
-  | Bishop of Color.t
-  | Rook of Color.t
-  | Queen of Color.t
-  | King of Color.t
-[@@deriving sexp]
+module Kind : sig
+  type t
+end
+
+type t = { kind : Kind.t; color : Color.t } [@@deriving sexp, fields ~getters]
 
 val to_ascii : t -> string
