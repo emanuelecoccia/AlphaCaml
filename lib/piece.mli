158f898349e5874109bf81082d86bcb48f27171d
Emanuele Coccia <emanuele.coccia@studbocconi.it>
2025-11-10 11:56:05 +0100

    exposing kind for piece type

--- lib/piece.mli
+++ lib/piece.mli
@@ -1,5 +1,5 @@
 module Kind : sig
-  type t
+  type t = Pawn | Knight | Bishop | Rook | Queen | King [@@deriving sexp]
 end
 
 type t = { kind : Kind.t; color : Color.t } [@@deriving sexp, fields ~getters]
